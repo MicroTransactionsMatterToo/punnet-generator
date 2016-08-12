ad17b04fecf83343590e384f3a20e4fee410fbbe
Ennis Massey <ennisbaradine@gmail.com>
2016-08-13 02:38:11 +1200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name="pungen",
+      version="1.0",
+      author="Ennis Massey",
+      author_email="ennisbaradine@gmail.com",
+      description="A simple program to generate Punnet Squares from CLI",
+      scripts=["bin/pungen"],
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: Python 3 :: Only",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Utilities",
+          "Environment :: Console",
+          "Development Status :: 5 - Production/Stable"
+      ])
